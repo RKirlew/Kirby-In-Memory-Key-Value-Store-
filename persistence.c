9cc815f460ba62119e2c62144249fc410192f7f6
R_Kirlew <kirlewraheem@gmail.com>
2025-04-05 19:40:12 -0500

    Update persistence.c

--- persistence.c
+++ persistence.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
+
+
+extern Entry* head; 
 void load_from_file(const char* filename) {
     FILE* fp = fopen(filename, "r");
     if (!fp) return;
@@ -17,8 +20,7 @@ void load_from_file(const char* filename) {
 void save_to_file(const char* filename) {
     FILE* fp = fopen(filename, "w");
     if (!fp) return;
-
-    Entry* head; // if needed, use get_all_entries()
+    
     Entry* curr = head;
     while (curr) {
         fprintf(fp, "%s %s\n", curr->key, curr->value);
